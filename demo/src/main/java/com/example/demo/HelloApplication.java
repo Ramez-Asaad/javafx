ab5d9ed0acb190c269116fbdeed0bd4e7dd8d2f3
edooooooooooooooooooo22 <eyadelnakib@yahoo.com>
2024-08-15 16:54:31 +0300

    NENEBB

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -18,6 +18,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
         System.out.println("ddd");
+        System.out.println("hagbahfvejhvf");
     }
 
     public static void main(String[] args) {
